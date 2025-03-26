fce1291f0c44b7a790d47c579a118404776a45d8
Del07-m <martinsdel07@gmail.com>
2025-03-26 11:52:38 +0100

    my new modification

--- myMainProject/ecommerce/urls.py
+++ myMainProject/ecommerce/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.storePage),
+    path('register',views.register),
+    # path('',views.signIn),
+
 ]
--- myMainProject/ecommerce/views.py
+++ myMainProject/ecommerce/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import login
+from .form import RegistrationForm
+from django.contrib.auth.models import User
+
 
 from . models import *
+
+
 
 # Create your views here.
 def storePage(request):
@@ -10,3 +17,26 @@ def storePage(request):
 
 
     return render (request, 'ecom/store.html',{'objects': products})
+
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect("login")
+
+       
+
+# return redirect('store')  
+
+
+
+    form = RegistrationForm()
+    return render(request, template_name="ecom/signup.html", context={"form":form})
+
+
+username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
